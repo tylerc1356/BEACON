8d2df30a7372980a1f6458f06cbdf94e468118b4
tylerc1356 <tydog1229@icloud.com>
2026-06-28 13:51:22 -0400

    Add reusable Power information module

---
+++ Modules/Hardware/PowerInfo.psm1
@@ -0,0 +1,20 @@
+﻿function Get-BEPowerInfo {
+    $battery = Get-CimInstance Win32_Battery -ErrorAction SilentlyContinue
+
+    $powerPlan = try {
+        powercfg /getactivescheme 2>$null
+    }
+    catch {
+        "Unavailable"
+    }
+
+    [PSCustomObject]@{
+        ActivePowerPlan          = $powerPlan
+        BatteryPresent           = if ($battery) { $true } else { $false }
+        BatteryStatus            = if ($battery) { $battery.BatteryStatus } else { "N/A" }
+        EstimatedChargeRemaining = if ($battery) { $battery.EstimatedChargeRemaining } else { "N/A" }
+        EstimatedRuntimeMinutes  = if ($battery) { $battery.EstimatedRunTime } else { "N/A" }
+    }
+}
+
+Export-ModuleMember -Function Get-BEPowerInfo
