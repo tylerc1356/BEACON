089eea3140ed7583ab17beb772023b75f91077f6
tylerc1356 <tydog1229@icloud.com>
2026-06-28 13:56:07 -0400

    Add reusable Services information module

---
+++ Modules/OperatingSystem/ServicesInfo.psm1
@@ -0,0 +1,28 @@
+﻿function Get-BEServicesInfo {
+    $importantServiceNames = @(
+        "wuauserv",
+        "WinDefend",
+        "Spooler",
+        "WinRM",
+        "BITS",
+        "EventLog",
+        "LanmanWorkstation",
+        "LanmanServer"
+    )
+
+    foreach ($serviceName in $importantServiceNames) {
+        $service = Get-Service -Name $serviceName -ErrorAction SilentlyContinue
+        $serviceCim = Get-CimInstance Win32_Service -Filter "Name='$serviceName'" -ErrorAction SilentlyContinue
+
+        if ($service) {
+            [PSCustomObject]@{
+                Name        = $service.Name
+                DisplayName = $service.DisplayName
+                Status      = $service.Status
+                StartType   = if ($serviceCim) { $serviceCim.StartMode } else { "Unavailable" }
+            }
+        }
+    }
+}
+
+Export-ModuleMember -Function Get-BEServicesInfo
