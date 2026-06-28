be4f6adebc2c47f1e6653a219f82d23074901fed
tylerc1356 <tydog1229@icloud.com>
2026-06-28 14:13:04 -0400

    Add reusable event log information module

---
+++ Modules/OperatingSystem/EventLogInfo.psm1
@@ -0,0 +1,31 @@
+﻿function Get-BEEventLogInfo {
+    $eventStartTime = (Get-Date).AddHours(-24)
+
+    $criticalEvents = Get-WinEvent -FilterHashtable @{
+        LogName   = "System"
+        Level     = 1
+        StartTime = $eventStartTime
+    } -ErrorAction SilentlyContinue
+
+    $errorEvents = Get-WinEvent -FilterHashtable @{
+        LogName   = "System"
+        Level     = 2
+        StartTime = $eventStartTime
+    } -ErrorAction SilentlyContinue
+
+    $recentProblemEvents = Get-WinEvent -FilterHashtable @{
+        LogName   = "System"
+        Level     = 1,2
+        StartTime = $eventStartTime
+    } -ErrorAction SilentlyContinue |
+    Select-Object -First 10 TimeCreated, ProviderName, Id, LevelDisplayName, Message
+
+    [PSCustomObject]@{
+        TimeWindowHours     = 24
+        CriticalEventCount  = if ($criticalEvents) { $criticalEvents.Count } else { 0 }
+        ErrorEventCount     = if ($errorEvents) { $errorEvents.Count } else { 0 }
+        RecentProblemEvents = $recentProblemEvents
+    }
+}
+
+Export-ModuleMember -Function Get-BEEventLogInfo
