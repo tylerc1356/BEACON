e1b8cabf44a8e4e5c73a3f9c01c77842f151d50b
tylerc1356 <tydog1229@icloud.com>
2026-06-28 14:16:01 -0400

    Add reusable update information module

---
+++ Modules/OperatingSystem/UpdateInfo.psm1
@@ -0,0 +1,16 @@
+﻿function Get-BEUpdateInfo {
+    $hotfixes = Get-HotFix |
+        Sort-Object InstalledOn -Descending
+
+    $latestHotfix = $hotfixes | Select-Object -First 1
+
+    [PSCustomObject]@{
+        LatestHotFixID      = if ($latestHotfix) { $latestHotfix.HotFixID } else { "Unavailable" }
+        LatestDescription   = if ($latestHotfix) { $latestHotfix.Description } else { "Unavailable" }
+        LatestInstalledOn   = if ($latestHotfix) { $latestHotfix.InstalledOn } else { "Unavailable" }
+        LatestInstalledBy   = if ($latestHotfix) { $latestHotfix.InstalledBy } else { "Unavailable" }
+        RecentHotFixes      = $hotfixes | Select-Object -First 5
+    }
+}
+
+Export-ModuleMember -Function Get-BEUpdateInfo
